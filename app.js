378d01f6ec9dc12142f82abc9c634689ed41080c
Quy Tran <quytran288@gmail.com>
2017-08-17 17:26:34 -0500

    Adding comment for app.js file

--- app.js
+++ app.js
@@ -10,17 +10,20 @@ var appRoutes = require('./routes/app');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views')); // Setup folder view
+app.set('view engine', 'hbs'); // How to render the view
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// How to render the body
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// End how to render the body
+app.use(express.static(path.join(__dirname, 'public'))); // Public folder that can access from internet
 
+// Configuration for Header when accessing from another server
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -28,9 +31,10 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use('/', appRoutes);
+app.use('/', appRoutes); // Every request will be handled by appRoutes
 
 // catch 404 and forward to error handler
+// Render page if catch 404, here we render the index (Angular app).
 app.use(function (req, res, next) {
     return res.render('index');
 });
