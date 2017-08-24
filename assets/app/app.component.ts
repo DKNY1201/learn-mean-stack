57f4b8cdbe23c460759cf4a3cece5cbb64e16ed2
Quy Tran <quytran288@gmail.com>
2017-08-23 21:58:39 -0500

    Using property binding

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {Message} from "./message/message.model";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-
+    message: Message = new Message('New message', 'Quy');
 }
