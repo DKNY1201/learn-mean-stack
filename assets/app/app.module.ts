e474fe9391c0374049f2729a5c5e761dbebf6fee
Quy Tran <quytran288@gmail.com>
2017-08-24 14:04:57 -0500

    Implement input component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from "./app.component";
 import {FormsModule} from "@angular/forms";
 import {MessageComponent} from "./message/message.component";
 import {MessageListComponent} from "./message/message-list.component";
+import {MessageInputComponent} from "./message/message-input.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
-        MessageListComponent
+        MessageListComponent,
+        MessageInputComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
