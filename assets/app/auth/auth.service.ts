c8745bc266c416ac62ffd8d4e0f912af8a5e64c5
Quy Tran <quytran288@gmail.com>
2017-08-28 12:26:19 -0500

    Implement create User on frontend side

---
+++ assets/app/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from "@angular/core";
+import {Http, Headers, Response} from "@angular/http";
+import 'rxjs/Rx';
+import {Observable} from "rxjs/Observable";
+
+import {User} from "./user.model";
+
+@Injectable()
+export class AuthService{
+    serverUrl = 'http://localhost:3200/user';
+    constructor(private http: Http) {}
+
+    signup(user: User) {
+        const headers = new Headers({'Content-Type': 'application/json'});
+        const body = JSON.stringify(user);
+        return this.http.post(this.serverUrl, body, {headers: headers})
+            .map((response: Response) => {response.json()})
+            .catch((error: Response) => Observable.throw(error.json()));
+    }
+
+}
