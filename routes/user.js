33ec306878289270a894144fcd8130355f1af346
Quy Tran <quytran288@gmail.com>
2017-08-28 21:05:06 -0500

    Implement routing for log in

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
@@ -26,5 +27,37 @@ router.post('/', function (req, res, next) {
         });
     });
 });
+
+router.post('/signin', function (req, res, next) {
+    User.findOne({email: req.body.email}, function (err, user) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred!',
+                err: err
+            });
+        }
+
+        if (!user) {
+            return res.status(401).json({
+                title: 'Login failed',
+                err: {message: 'Invalid login credentials'}
+            });
+        }
+
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            return res.status(401).json({
+                title: 'Login failed',
+                err: {message: 'Invalid login credentials'}
+            });
+        }
+
+        var token = jwt.sign({'user': user}, 'secret', {expiresIn: 7200});
+        res.status(200).json({
+            message: 'Successfully logged in',
+            token: token,
+            userId: user._id
+        });
+    });
+})
 
 module.exports = router;
